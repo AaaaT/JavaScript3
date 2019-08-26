3565626fdc9208f957ea9af771ec2b4e1b2c5758
unknown <toegelanna@outlook.com>
2019-08-26 22:40:14 +0200

    removed the old function

--- homework/index.js
+++ homework/index.js
@@ -1,21 +1,6 @@
 'use strict';
 
 {
-  // function fetchJSON(url, cb) {
-  //   const xhr = new XMLHttpRequest();
-  //   xhr.open('GET', url);
-  //   xhr.responseType = 'json';
-  //   xhr.onload = () => {
-  //     if (xhr.status < 400) {
-  //       cb(null, xhr.response);
-  //     } else {
-  //       cb(new Error(`Network error: ${xhr.status} - ${xhr.statusText}`));
-  //     }
-  //   };
-  //   xhr.onerror = () => cb(new Error('Network request failed'));
-  //   xhr.send();
-  // }
-
   function fetchJSON(url) {
     return new Promise((resolve, reject) => {
       const xhr = new XMLHttpRequest();
